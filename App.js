3a6a8243ae78779ff0fdf5334ac53d0a8445dbf3
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-03-22 23:03:11 +0700

    Add gradient

--- App.js
+++ App.js
@@ -1,5 +1,17 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={["#4f042a", "#ddb52f"]} style={styles.rootScreen}>
+      <StartGameScreen />
+    </LinearGradient>
+  );
 }
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+});
